24218d5d54970ca753bf3dd04dd5128e0898c6bd
PNNutkung <pipatpol.t@ku.th>
2016-12-25 22:43:11 +0700

    :zap: Add create Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
   def new
   end
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
